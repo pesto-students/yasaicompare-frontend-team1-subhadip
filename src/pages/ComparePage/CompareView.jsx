22f7aa021c359b5894c60bde9f030213226cbcff
Dibya <Dibya.DebayanDash@siriuscom.com>
2023-02-24 16:31:59 +0530

    Made small changes

--- src/pages/ComparePage/CompareView.jsx
+++ src/pages/ComparePage/CompareView.jsx
@@ -1,17 +1,17 @@
-import GroceryCard from "../../components/GroceryCard/GroceryCard";
+import ItemCard from "../../components/ItemCard/ItemCard";
 import { SimpleGrid } from "@chakra-ui/react";
 
 const CompareView = () => {
   return (
     <SimpleGrid columns={[2, 2, 4, 6, 8]} gap="10px">
-      <GroceryCard />
-      <GroceryCard />
-      <GroceryCard />
-      <GroceryCard />
-      <GroceryCard />
-      <GroceryCard />
-      <GroceryCard />
-      <GroceryCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
     </SimpleGrid>
   );
 };
