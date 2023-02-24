e0e885b7c1b2bd730333bbb3ddb8836f0764683a
Dibya <Dibya.DebayanDash@siriuscom.com>
2023-02-24 16:18:49 +0530

    Added some minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from "react";
 
 export const ShopPage = React.lazy(() => import("./ShopPage/ShopPage"));
 
+export const ComparePage = React.lazy(() => import("./ComparePage/ComparePage"));
+
 // export const SomeOtherPage = React.lazy(() =>
 //   import("./SomeOtherPage/SomeOtherPage")
 // );
