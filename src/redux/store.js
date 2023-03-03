9565a8b3b9ac5ae2a628a01df468f9b2c04f01f9
Dibya <Dibya.DebayanDash@siriuscom.com>
2023-03-03 19:06:18 +0530

    Added redux logic for the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { createLogger } from "redux-logger";
 
 import authReducer from "./features/auth/authSlice";
-import shopReducer from "./features/shop/shopSlice";
+import shopReducer from "./features/auth/shopSlice";
 
 const logger = createLogger();
 
