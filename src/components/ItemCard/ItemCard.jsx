6fadd03c3b95da706964943cfe400a391f7195be
Dibya <Dibya.DebayanDash@siriuscom.com>
2023-03-07 15:39:10 +0530

    Added button functionality and integrated items in itempage

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -7,15 +7,73 @@ import {
   Button,
   Stack,
   CardBody,
-  IconButton,
-  Flex,
-  ButtonGroup,
+  HStack,
+  Input,
 } from "@chakra-ui/react";
 import { formatPrice } from "../../utils/commons";
 import PropTypes from "prop-types";
-import { AddIcon, SubtractIcon } from "../Icons";
+import { useNumberInput } from "@chakra-ui/react";
 
+// export function ItemCards(props) {
+//   return (
+//     <Card marginLeft='1' marginTop = '1'display="flex" justifyContent="center" width="180px" h="110px" boxShadow='xl'>
+//       <Box display="flex" justifyContent="space-between" p='3'>
+//         <Image src={paprika} alt="paprika" w="50px" h="50px" borderRadius='6px' />
+//         <Heading fontSize="18" fontWeight="bold">
+//           Red Paprika
+//         </Heading>
+//       </Box>
+//       <Box display="flex" justifyContent="space-between" p='2'>
+//         <Box display="flex">
+//           <Text fontSize="14px" color="green.500" fontWeight="bold">
+//             ₹75.00
+//           </Text>
+//           <Text fontSize="14px" color="gray.500" fontWeight="bold">
+//             /kg
+//           </Text>
+//         </Box>
+//         <Button
+//           size="xs"
+//           variant="outline"
+//           borderRadius="20px"
+//           border="2px"
+//           p="2"
+//           fontSize="14px"
+//         >
+//           Add 2 Cart
+//         </Button>
+//       </Box>
+//     </Card>
+//   );
+// }
+function NumberButton() {
+  const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
+    useNumberInput({
+      step: 1,
+      defaultValue: 1,
+      min: 1,
+      max: 6,
+      precision: 0,
+    });
+
+  const inc = getIncrementButtonProps();
+  const dec = getDecrementButtonProps();
+  const input = getInputProps();
+
+  return (
+    <HStack maxW="320px">
+      <Button h="6" {...inc}>
+        +
+      </Button>
+      <Input w="20" {...input} />
+      <Button h="6" {...dec}>
+        -
+      </Button>
+    </HStack>
+  );
+}
 export const ItemCard = (props) => {
+  const [buttonclicked, setButtonClicked] = React.useState(true);
   const imageSize = !props.minimal ? "100px" : "50px";
   return (
     <Card size={"sm"}>
@@ -41,35 +99,17 @@ export const ItemCard = (props) => {
                 <Text fontSize="12px">{formatPrice(props.price)}</Text>
               </Stack>
               <Stack flex="1">
-                {!props.quantity ? (
+                {buttonclicked ? (
                   <Button
                     size="xs"
                     borderRadius="8px"
                     width="full"
-                    onClick={props.onIncrementClick}
+                    onClick={() => setButtonClicked(false)}
                   >
                     Add
                   </Button>
                 ) : (
-                  <ButtonGroup
-                    borderRadius="6"
-                    color="white"
-                    bgColor="green.500"
-                    size="xs"
-                    width="full"
-                  >
-                    <IconButton
-                      icon={<SubtractIcon />}
-                      onClick={props.onDecrementClick}
-                    />
-                    <Flex justifyContent="center" alignItems="center">
-                      <Text fontSize="xs">{props.quantity}</Text>
-                    </Flex>
-                    <IconButton
-                      icon={<AddIcon />}
-                      onClick={props.onIncrementClick}
-                    />
-                  </ButtonGroup>
+                  <NumberButton />
                 )}
               </Stack>
             </Stack>
