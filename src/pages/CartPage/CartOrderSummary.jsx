6564f2ef2f59da24c9c490d87f8391cda856c8dc
Dibya <Dibya.DebayanDash@siriuscom.com>
2023-03-19 11:42:34 +0530

    Added necessary change

--- src/pages/CartPage/CartOrderSummary.jsx
+++ src/pages/CartPage/CartOrderSummary.jsx
@@ -46,7 +46,7 @@ export const CartOrderSummary = ({ totalcartitems }) => {
   const prepareOrderData = () => {
     let finalData = {
       orders: [],
-      delievery_address: "",
+      delievery_address: selectedAddress?.id || '',
     };
 
     cartDataState.data.forEach((cartItem) => {
